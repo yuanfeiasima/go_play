986a3f6030ebc54a434d141d56c80e67c28b366f
yuanfeiasima <382810567@qq.com>
2016-10-25 18:05:57 +0800

    http2

---
+++ src/mathapp/model/model.go
@@ -0,0 +1,20 @@
+package model
+
+type Human struct {
+	name string
+	age int
+	phone string
+}
+
+type Student struct {
+	Human //匿名字段human
+	school string
+	loan float32
+}
+
+type Employee struct {
+	Human
+	company string
+	money float32
+}
+
