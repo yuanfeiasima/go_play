ace1ef3bc555da6966c8f8fa5bc7142f1d62aff1
yuanfeiasima <382810567@qq.com>
2016-10-25 10:31:56 +0800

    http2

--- src/mathapp/main.go
+++ src/mathapp/main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"fmt"
 	//"mymath"
-	"net/http"
 	"github.com/drone/routes"
+	"net/http"
 )
 
-func getuser(w http.ResponseWriter, r *http.Request){
+func getuser(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 	uid := params.Get(":uid")
 
-	fmt.Fprintln(w, "you are get user %s", uid)
+	if uid == "lulu" {
+		fmt.Fprintf(w, "beautiful girl  %s", uid)
+	} else {
+		fmt.Fprintf(w, "you are get user %s", uid)
+	}
+
 }
 func main() {
 	//fmt.Println("Hello, world. Sqrt(2) = %v\n", mymath.Sqrt(2))
+	fmt.Println("mathapp start!")
 	mux := routes.New()
 	mux.Get("/user/:uid", getuser)
 	http.Handle("/", mux)
