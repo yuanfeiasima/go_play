a79f4e8b2dd1d1f5f75881c3565aa89ce8c40270
yuanfeiasima <382810567@qq.com>
2016-12-14 10:32:33 +0800

    reflect

--- src/ThreadGo/thread.go
+++ src/ThreadGo/thread.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 var counter int = 0
 
@@ -10,14 +13,25 @@ func Count(ch chan int) {
 }
 
 func main() {
-	chs := make([]chan int, 10)
-	for i := 0; i < 10; i++ {
-		chs[i] = make(chan int)
-		go Count(chs[i])
-	}
+	//chs := make([]chan int, 10)
+	//for i := 0; i < 10; i++ {
+	//	chs[i] = make(chan int)
+	//	go Count(chs[i])
+	//}
+	//
+	//for _, ch := range chs {
+	//	i := <-ch
+	//	fmt.Println(i)
+	//}
+	//ThreadGo.GetCpuNUm()
+	multiThread(2)
+}
 
-	for _, ch := range chs {
-		i := <-ch
-		fmt.Println(i)
+//设置GOMAXPROCS 充分利用多核的优势 真正的并发执行
+func multiThread(cpuNum int) {
+	runtime.GOMAXPROCS(cpuNum)
+	for {
+		fmt.Print(1)
+		go fmt.Print(0)
 	}
 }
